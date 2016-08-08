a8f6ea5e8be309e0d17c67323fe9eda48a524e76
nadine-shi <nadine@afterlogic.com>
2016-08-08 13:39:09 +0300

    Create module StandardAuthWebclient with admin and user settings tabs

--- js/manager.js
+++ js/manager.js
@@ -8,61 +8,32 @@ module.exports = function (oAppData, iUserRole, bPublic) {
 	var
 		_ = require('underscore'),
 		
-		TextUtils = require('modules/CoreClient/js/utils/Text.js'),
 		Types = require('modules/CoreClient/js/utils/Types.js'),
 		
 		Settings = require('modules/%ModuleName%/js/Settings.js'),
 		oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {}),
 		
-		bAnonimUser = iUserRole === Enums.UserRole.Anonymous,
-		bAdminUser = iUserRole === Enums.UserRole.SuperAdmin,
-		bPowerUser = iUserRole === Enums.UserRole.PowerUser
+		bAnonimUser = iUserRole === Enums.UserRole.Anonymous
 	;
 	
 	Settings.init(oSettings);
 	
-	return {
-		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic;
-		},
-		/**
-		 * Runs after app initializing. Adds basic accounts tab to admin panel.
-		 * 
-		 * @param {Object} ModulesManager Modules manager object.
-		 */
-		start: function (ModulesManager) {
-			if (bAdminUser)
-			{
-				ModulesManager.run('AdminPanelClient', 'registerAdminPanelTab', [
-					function () { return require('modules/%ModuleName%/js/views/AccountsSettingsView.js'); },
-					Settings.HashModuleName + '-accounts',
-					TextUtils.i18n('%MODULENAME%/LABEL_BASIC_ACCOUNTS_TAB')
-				]);
-			}
-			if (bPowerUser)
-			{
-				ModulesManager.run('SettingsClient', 'registerSettingsTab', [
-					function () { return require('modules/%ModuleName%/js/views/AccountsSettingsView.js'); },
-					Settings.HashModuleName + '-accounts',
-					TextUtils.i18n('%MODULENAME%/LABEL_BASIC_ACCOUNTS_TAB')
-				]);
-			}
-		},
-		getScreens: function () {
-			var oScreens = {};
-			if (bAnonimUser)
-			{
+	if (!bPublic && bAnonimUser)
+	{
+		return {
+			getScreens: function () {
+				var oScreens = {};
 				oScreens[Settings.HashModuleName] = function () {
 					return require('modules/%ModuleName%/js/views/WrapLoginView.js');
 				};
+				return oScreens;
+			},
+			beforeAppRunning: function () {
+				if (Types.isNonEmptyString(Settings.CustomLoginUrl))
+				{
+					window.location.href = Settings.CustomLoginUrl;
+				}
 			}
-			return oScreens;
-		},
-		beforeAppRunning: function (bAuth) {
-			if (!bAuth && Types.isNonEmptyString(Settings.CustomLoginUrl))
-			{
-				window.location.href = Settings.CustomLoginUrl;
-			}
-		}
-	};
+		};
+	}
 };
