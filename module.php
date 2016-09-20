096fa773283ed990beabfef8e3f2d583254bf374
nadine-shi <nadine@afterlogic.com>
2016-09-20 12:54:09 +0300

    test

--- module.php
+++ module.php
@@ -16,6 +16,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
+//test
+
 class StandardLoginFormWebclientModule extends AApiModule
 {
 	protected $aSettingsMap = array(
