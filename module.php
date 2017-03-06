12455d5d2ba92f64da9d973041867414fc385a24
sash04ek <a.ovcharov@gmail.com>
2017-03-06 08:55:02 +0300

    refactoring namespaces

--- module.php
+++ module.php
@@ -18,9 +18,9 @@
  * @package Modules
  */
 
-namespace Aurora\Modules;
+namespace Aurora\Modules\StandardLoginFormWebclient;
 
-class StandardLoginFormWebclientModule extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractModule
 {
 	/***** public functions might be called with web API *****/
 	/**
