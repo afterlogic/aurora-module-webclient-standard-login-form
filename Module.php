ff3c13a64b870adf8529c0c72bd046462e4344d0
vasil <vasil.sokolov@gmail.com>
2017-03-06 11:58:54 +0300

    Second stem of renaming of modules.php

---
+++ Module.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ * 
+ * @package Modules
+ */
+
+namespace Aurora\Modules\StandardLoginFormWebclient;
+
+class Module extends \Aurora\System\Module\AbstractModule
+{
+	/***** public functions might be called with web API *****/
+	/**
+	 * Obtains list of module settings for authenticated user.
+	 * 
+	 * @return array
+	 */
+	public function GetSettings()
+	{
+		\Aurora\System\Api::checkUserRoleIsAtLeast(\EUserRole::Anonymous);
+		
+		return array(
+			'ServerModuleName' => $this->getConfig('ServerModuleName', ''),
+			'HashModuleName' => $this->getConfig('HashModuleName', ''),
+			'CustomLoginUrl' => $this->getConfig('CustomLoginUrl', ''),
+			'CustomLogoUrl' => $this->getConfig('CustomLogoUrl', ''),
+			'DemoLogin' => $this->getConfig('DemoLogin', ''),
+			'DemoPassword' => $this->getConfig('DemoPassword', ''),
+			'InfoText' => $this->getConfig('InfoText', ''),
+			'LoginSignMeType' => $this->getConfig('LoginSignMeType', 0),
+		);
+	}
+	
+	public function Login($Login, $Password, $SignMe)
+	{
+		return \Aurora\System\Api::GetModuleDecorator('Core')->Login($Login, $Password, $SignMe);
+	}
+	/***** public functions might be called with web API *****/
+}
